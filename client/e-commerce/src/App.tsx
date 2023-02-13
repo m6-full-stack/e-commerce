08a5abb837663285e6caa304f8eb2ac5845ce2b5
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-02-13 18:37:18 -0300

    feat: styles
    criação da estilização dos parágrafos.

--- client/e-commerce/src/App.tsx
+++ client/e-commerce/src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from "styled-components"
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
+import { ThemeParagraph, ThemeTitle } from "./styles/typography"
+
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>teste</h1>
+      {/* <ThemeTitle tag='h1' titleSize='title1-700' color='blue'>teste</ThemeTitle> */}
+      {/* <ThemeParagraph paragraphSize='body-1-400' color='blue'>teste</ThemeParagraph> */}
       <GlobalStyle/>
     </ThemeProvider>
   )
--- client/e-commerce/src/styles/global.ts
+++ client/e-commerce/src/styles/global.ts
@@ -8,6 +8,6 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: red;
+    
   }
 `
