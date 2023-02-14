adce2eba554718f658b23803c86084ab149f4b36
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-02-13 22:06:22 -0300

    feat: styles
    configuração do layout

---
+++ client/e-commerce/src/Router/Router.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from '../components/layouts/Default.layout'
+import { Home } from '../pages/Home'
+import { Login } from '../pages/Login'
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Route>
+    </Routes>
+  )
+}
