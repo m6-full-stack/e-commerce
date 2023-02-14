87ccf252738a134bed1d94ee1e2c02276db932ad
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-02-14 13:29:26 -0300

    feat: styles
    adicionado estilização no global.ts

--- client/e-commerce/src/styles/global.ts
+++ client/e-commerce/src/styles/global.ts
@@ -4,11 +4,40 @@ export const GlobalStyle = createGlobalStyle`
   *{
     margin: 0;
     padding: 0;
+    outline: 0;
     box-sizing: border-box;
     
   }
 
   body {
-    /* background-color: ${(props) => props.theme.grey10}; */
+    background: ${(props) => props.theme.grey0};
   }
+
+  button {
+		cursor: pointer;
+    border: 0;
+    background: transparent;
+	}
+
+  ul, ol, li{
+        list-style: none;
+        
+    }
+    
+  a {
+		text-decoration: none;
+		cursor: pointer;
+	}
+
+  img {
+        max-width: 100%;
+    }
+
+  section, aside, div{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: flex-start;
+    }
 `
