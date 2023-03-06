6abb8bd4ae60013f09cc619316a6c51c497c31d5
Pedro Gabriel <pedrogv03@hotmail.com>
2023-03-06 10:06:08 -0300

    fix: useParams bug

--- client/e-commerce/src/pages/Product/index.tsx
+++ client/e-commerce/src/pages/Product/index.tsx
@@ -1,19 +1,18 @@
-import { useContext, useEffect} from "react";
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { AsideProducts } from "../../components/AsideProducts";
 import { FooterProducts } from "../../components/FooterProducts";
 import { HeaderProductsStyle } from "../../components/HeaderProducts/style";
 import { MainProducts } from "../../components/MainProducts";
-
 import { ProductContainer } from "../../components/Products/style";
 import { SectionProducts } from "../../components/SectionProducts";
 import { AnnouncementContext } from "../../contexts/AnnouncementProvider/AnnouncementProvide";
 
 export const Product = () => {
   const { getRetriveAnnouncement } = useContext(AnnouncementContext);
-
+  const { id } = useParams();
+  
   useEffect(() => {
-    const { id } = useParams();
     id && getRetriveAnnouncement(id);
   }, []);
 
